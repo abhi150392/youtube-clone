d30229834729123e37037ef870be5bdaae8a7a11
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-10-03 16:31:56 -0500

    Implemented Debouncing to limit API calls

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,37 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
-import { ICON_URL, LOGO_URL, MENU_IMG_URL } from "../utils/constants";
+import {
+  ICON_URL,
+  LOGO_URL,
+  MENU_IMG_URL,
+  YOUTUBE_SEARCH_API,
+} from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  //De-Bouncing--Make an API call if diffrence between two key strokes is > 200ms
+  useEffect(() => {
+    //make an API call after every key press
+    //if diffr between 2 api calls < 200ms
+    //decline API call
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    //API call
+
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    console.log(json[1]);
+  };
+
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
@@ -23,6 +50,8 @@ const Header = () => {
         <input
           className="w-1/2 border border-gray-400 p-2 rounded-l-full"
           type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button className="border border-gray-400 p-2 rounded-r-full">
           Search
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,5 +10,8 @@ export const MENU_IMG_URL =
 export const ICON_URL =
   "https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg";
 
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
 export const LOGO_URL =
   "https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500";
